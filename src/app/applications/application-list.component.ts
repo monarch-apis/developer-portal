70e06ca22af337c6792cfba136d8e3b275db4e0a
Francis Malone <fmalone@mbp-fmalone02.local>
2017-09-07 16:15:17 -0400

    Basic Applications page working

--- src/app/applications/application-list.component.ts
+++ src/app/applications/application-list.component.ts
@@ -1,8 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ApplicationService } from './application.service';
+import { Application } from './application';
 
 @Component({
   templateUrl: './application-list.component.html',
   styleUrls: [ './application-list.component.css' ],
 })
-export class ApplicationListComponent {
+export class ApplicationListComponent implements OnInit {
+
+  constructor(private _applicationService: ApplicationService) {
+
+  }
+
+  applications:Application[] = []
+
+  ngOnInit() {
+    this._applicationService.getApplication().subscribe(
+      (body) => {
+        this.applications = body
+        console.log('sucess')
+      },
+      error => {
+        console.log('fail')
+      }
+    )
+  }
 }
